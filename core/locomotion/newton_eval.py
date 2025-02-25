8bcdff42de3d810c81b1534cc2fec0ef98a57dc6
shamiivan <shamiivan@gmail.com>
2025-02-25 14:42:36 -0500

    fix: Update eval to use newton env instead of go2

--- core/locomotion/newton_eval.py
+++ core/locomotion/newton_eval.py
@@ -3,7 +3,7 @@ import os
 import pickle
 
 import torch
-from go2_env import Go2Env
+from newton_env import NewtonEnv
 from rsl_rl.runners import OnPolicyRunner
 
 import genesis as gs
@@ -11,7 +11,7 @@ import genesis as gs
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-e", "--exp_name", type=str, default="go2-walking")
+    parser.add_argument("-e", "--exp_name", type=str, default="newton-walking")
     parser.add_argument("--ckpt", type=int, default=100)
     args = parser.parse_args()
 
@@ -21,7 +21,7 @@ def main():
     env_cfg, obs_cfg, reward_cfg, command_cfg, train_cfg = pickle.load(open(f"logs/{args.exp_name}/cfgs.pkl", "rb"))
     reward_cfg["reward_scales"] = {}
 
-    env = Go2Env(
+    env = NewtonEnv(
         num_envs=1,
         env_cfg=env_cfg,
         obs_cfg=obs_cfg,
