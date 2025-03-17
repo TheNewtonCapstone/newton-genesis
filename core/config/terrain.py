a81f31a8e640bd54a7c13e85beaa9b2bd14b113d
Houssam E. Righi <hrighi01@gmail.com>
2025-03-16 23:57:48 -0400

    Increased the horizontal_scale for better stairs

--- core/config/terrain.py
+++ core/config/terrain.py
@@ -3,7 +3,7 @@ class TerrainConfig:
     # Mesh and Scaling Configuration
     # ---------------------------------------------------------------------------
     mesh_type = 'trimesh'  # Options: 'none', 'plane', 'heightfield', or 'trimesh'
-    horizontal_scale = 0.1  # Horizontal scale [m]
+    horizontal_scale = 0.05  # Horizontal scale [m]
     vertical_scale = 0.005  # Vertical scale [m]
     border_size = 0.1      # Border size [m]
 
@@ -32,18 +32,18 @@ class TerrainConfig:
 
     # Terrain dimensions and layout configuration
     terrain_size = 8.0  # Terrain length [m]
-    num_rows = 3       # Number of terrain rows (levels)
-    num_cols = 3       # Number of terrain columns (types)
+    num_rows = 3      # Number of terrain rows (levels)
+    num_cols = 4       # Number of terrain columns (types)
 
     # Terrain type proportions:
     # [smooth slope, rough slope, stairs up, stairs down, discrete, stepping_stones]
     terrain_proportions = [0.1, 0.1, 0.35, 0.45, 0.7]
-    terrain_types = ['smooth_slope', 'stairs_up', 'stairs_down', 'discrete', 'stepping_stones']
+    terrain_types = ['smooth_slope', 'stairs_down', 'stairs_up', 'discrete', 'stepping_stones']
 
     # ---------------------------------------------------------------------------
     # Trimesh Specific Settings
     # ---------------------------------------------------------------------------
-    slope_threshold = 0.75  # For 'trimesh' type: slopes above this threshold will be corrected to vertical surfaces
+    slope_threshold = 0.45  # For 'trimesh' type: slopes above this threshold will be corrected to vertical surfaces
 
     # ---------------------------------------------------------------------------
     # Physical Properties
